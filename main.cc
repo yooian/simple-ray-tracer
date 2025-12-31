a9d2d612c1cbe49034509c475cf51e0c6d95718d
Ian Yoo <isyoo@ucdavis.edu>
2025-12-31 15:52:12 -0800

    Refactor main with color and vec3 headers

--- main.cc
+++ main.cc
@@ -1,27 +1,27 @@
+#include "vec3.h"
+#include "color.h"
+
 #include <iostream>
 
-int main() {
-    // Image
-    int image_width = 256;
-    int image_height = 256;
-    
-    // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+int main()
+{
+	// Image
+	int image_width = 256;
+	int image_height = 256;
 
-    for (int j = 0; j < image_height; ++j) {
-	std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-	for (int i = 0; i < image_width; ++i) {
-	    auto r = double(i) / (image_width - 1);
-	    auto g = double(j) / (image_height -1);
-	    auto b = 0.0;
+	// Render
+	std::cout << "P3\n"
+			  << image_width << ' ' << image_height << "\n255\n";
 
-	    int ir = int(255.999 * r);
-	    int ig = int(255.999 * g);
-	    int ib = int(255.999 * b);
-
-	    std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+	for (int j = 0; j < image_height; ++j)
+	{
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i)
+		{
+			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			write_color(std::cout, pixel_color);
+		}
 	}
-    }
 
-    std::clog << "\rDone.		\n";
+	std::clog << "\rDone.		\n";
 }
