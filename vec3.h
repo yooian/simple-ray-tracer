4bda30b6d5c916d245b2d29282305db0e19031df
Ian Yoo <isyoo@ucdavis.edu>
2025-12-31 15:02:17 -0800

    Complete vec3 class function definitions

--- vec3.h
+++ vec3.h
@@ -7,11 +7,52 @@
 class vec3
 {
 public:
+    double e[3];
+
     // constructor
+    vec3() : e{0, 0, 0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    // member functions
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    double length() const
+    {
+        return std::sqrt(length_squared());
+    }
+
+    double length_squared() const
+    {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
 
     // operator overloads
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double &operator[](int i) { return e[i]; }
 
-    // functions
+    vec3 &operator+=(const vec3 &v)
+    {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    vec3 &operator*=(double t)
+    {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec3 &operator/=(double t)
+    {
+        return *this *= 1 / t;
+    }
 };
 
 // point alias for vec3
