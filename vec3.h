249caa0fc1a63d48a239f05bebe5d812ef2b9044
Ian Yoo <isyoo@ucdavis.edu>
2025-12-31 15:35:22 -0800

    Add vector utility functions

--- vec3.h
+++ vec3.h
@@ -56,6 +56,59 @@ public:
 };
 
 // point alias for vec3
+using point3 = vec3;
 
 // Vector Utility Functions
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v)
+{
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v)
+{
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t)
+{
+    return t * v;
+}
+
+inline vec3 operator/(const vec3 &v, double t)
+{
+    return (1 / t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v)
+{
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3 &v)
+{
+    return v / v.length();
+}
+
 #endif
