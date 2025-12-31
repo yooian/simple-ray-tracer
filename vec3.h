85019c542dec3ade3a97a8c3b6ae631ad33fbfde
Ian Yoo <isyoo@ucdavis.edu>
2025-12-31 14:28:01 -0800

    Add vector pseudocode

--- vec3.h
+++ vec3.h
@@ -1,4 +1,20 @@
 #ifndef VEC3_H
 #define VEC3_H
 
+#include <cmath>
+#include <iostream>
+
+class vec3
+{
+public:
+    // constructor
+
+    // operator overloads
+
+    // functions
+};
+
+// point alias for vec3
+
+// Vector Utility Functions
 #endif
