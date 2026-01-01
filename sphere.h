96e4e9b949746ab1475d842b24f0eba9b5b5becf
Ian Yoo <isyoo@ucdavis.edu>
2026-01-01 14:25:36 -0800

    Add hit logic for sphere

--- sphere.h
+++ sphere.h
@@ -8,7 +8,36 @@ class sphere : public hittable
 {
 public:
     sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
-    bool hit() const override {}
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override
+    {
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius * radius;
+
+        auto discriminant = h * h - 4 * a * c;
+        if (discriminant < 0)
+            return false;
+
+        auto sqrtd = std::sqrt(discriminant);
+
+        // Find nearest root in acceptable range
+        auto root = (h - sqrtd) / a;
+        if (root <= ray_tmin || root >= ray_tmax)
+        {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || root >= ray_tmax)
+            {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = (rec.p - center) / radius;
+
+        return true;
+    }
 
 private:
     point3 center;
