fe3f1bb4b61f2e403f88acd6a7fb0f03cce091df
Ian Yoo <isyoo@ucdavis.edu>
2025-12-31 16:51:36 -0800

    Add scene rays

--- main.cc
+++ main.cc
@@ -1,7 +1,13 @@
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
 #include <iostream>
+
+color ray_color(const ray &r)
+{
+	return color(0, 0, 0);
+}
 
 int main()
 {
@@ -13,9 +19,25 @@ int main()
 	int image_height = int(image_width / aspect_ratio);
 	image_height = (image_height < 1) ? 1 : image_height;
 
+	// Camera
+	auto focal_length = 1.0;
+	auto camera_center = point3(0, 0, 0);
+
 	// Viewport calculation (VP is real-valued, so need actual aspec ratio calculated)
 	auto viewport_height = 2.0; // arbitrary
 	auto viewport_width = viewport_height * (double(image_width) / image_height);
+
+	// Horizontal/Vertical viewport edge vectors
+	auto viewport_u = vec3(viewport_width, 0, 0);
+	auto viewport_v = vec3(0, -viewport_height, 0);
+
+	// Horizontal/Vertical delta vectors from pixel to pixel
+	auto pixel_delta_u = viewport_u / image_width;
+	auto pixel_delta_v = viewport_v / image_height;
+
+	// Calculat location of upper-left pixel
+	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
 	// Render
 	std::cout << "P3\n"
@@ -26,7 +48,11 @@ int main()
 		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto ray_direction = pixel_center - camera_center;
+			ray r(camera_center, ray_direction);
+
+			color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
 		}
 	}
