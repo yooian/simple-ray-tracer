c1fa8cc210fa82e99ae2df70e64b153613cc73d2
Ian Yoo <isyoo@ucdavis.edu>
2025-12-31 16:09:50 -0800

    Add ray class

--- ray.h
+++ ray.h
@@ -1,4 +1,26 @@
 #ifndef RAY_H
 #define RAY_H
 
+#include "vec3.h"
+
+class ray
+{
+public:
+    ray() {}
+    ray(const point3 &origin, const vec3 &direction) : orig(origin), dir(direction) {}
+
+    const point3 &origin() const { return orig; }
+    const vec3 &direction() const { return dir; }
+
+    // 3D pos (P) on ray is function of P(t) = A + tb, A = origin, b = ray direction
+    point3 at(double t) const
+    {
+        return orig + t * dir;
+    }
+
+private:
+    point3 orig;
+    vec3 dir;
+};
+
 #endif
