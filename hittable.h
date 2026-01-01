d6ad307f8e6f9ab0cb6500581da67f12c01529ec
Ian Yoo <isyoo@ucdavis.edu>
2026-01-01 13:46:11 -0800

    Add hit record struct

--- hittable.h
+++ hittable.h
@@ -1,4 +1,15 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+#include "ray.h"
+
+// Struct to store information about hit
+class hit_record
+{
+public:
+    point3 p;
+    vec3 normal;
+    double t;
+};
+
 #endif
