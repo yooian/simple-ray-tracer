45bbe2246660acbcd1cd9605d2c7a35a69a14256
Ian Yoo <isyoo@ucdavis.edu>
2026-01-01 13:54:35 -0800

    Add hittable abstract base class

--- hittable.h
+++ hittable.h
@@ -12,4 +12,12 @@ public:
     double t;
 };
 
+class hittable
+{
+public:
+    virtual ~hittable() = default; // virtual destructor with default cleanup operation
+    virtual bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const = 0;
+    // const = 0 ensures hittable object cannot exist as itself, must be defined by children
+};
+
 #endif
