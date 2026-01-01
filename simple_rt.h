16482aaa4ba46aefd4b784c2e6468e1ffea36675
Ian Yoo <isyoo@ucdavis.edu>
2026-01-01 15:54:41 -0800

    Add constants, utility function, and common headers

--- simple_rt.h
+++ simple_rt.h
@@ -1,4 +1,28 @@
 #ifndef SIMPLE_RT_H
 #define SIMPLE_RT_H
 
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+// Can include usings, but commented out for clarity
+// using std::make_shared;
+// using std::shared_ptr;
+
+// Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+inline double degrees_to_radians(double degrees)
+{
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
 #endif
