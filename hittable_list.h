6e0f6a0280c25bff3a118cb8fcbca5232dca6e63
Ian Yoo <isyoo@ucdavis.edu>
2026-01-01 15:28:07 -0800

    Add initial functions and constructors

--- hittable_list.h
+++ hittable_list.h
@@ -1,6 +1,27 @@
 #ifndef HITTABLE_LIST_H
 #define HITTABLE_LIST_H
 
+#include "hittable.h"
+#include <memory>
+#include <vector>
 
+// using std::make_shared;
+// using std::shared_ptr;
+
+class hittable_list : public hittable
+{
+public:
+    // wouldnt be able to store hittable or hittable* since it's abstract
+    std::vector<std::shared_ptr<hittable>> objects;
+
+    hittable_list() {}
+    hittable_list(std::shared_ptr<hittable> object) { add(object); }
+
+    void clear() {}
+
+    void add() {}
+
+    bool hit() const override {}
+};
 
 #endif
