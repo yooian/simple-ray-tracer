abba00814aef1a0e3b105deb7fddf8a889a460e3
Ian Yoo <isyoo@ucdavis.edu>
2026-01-01 15:32:32 -0800

    Add hittable list hit logic

--- hittable_list.h
+++ hittable_list.h
@@ -17,11 +17,30 @@ public:
     hittable_list() {}
     hittable_list(std::shared_ptr<hittable> object) { add(object); }
 
-    void clear() {}
+    void clear() { objects.clear(); }
 
-    void add() {}
+    void add(std::shared_ptr<hittable> object)
+    {
+        objects.push_back(object);
+    }
 
-    bool hit() const override {}
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override
+    {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        auto closest_so_far = ray_tmax;
+
+        for (const auto &object : objects)
+        {
+            if (object->hit(r, ray_tmin, closest_so_far, temp_rec))
+            {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+        return hit_anything;
+    }
 };
 
 #endif
