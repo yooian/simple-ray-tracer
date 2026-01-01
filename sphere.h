173fbf9189aa67ece5158511eb4056cf67b99842
Ian Yoo <isyoo@ucdavis.edu>
2026-01-01 14:09:01 -0800

    Add sphere class and initial functions

--- sphere.h
+++ sphere.h
@@ -1,4 +1,18 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable
+{
+public:
+    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    bool hit() const override {}
+
+private:
+    point3 center;
+    double radius;
+};
+
 #endif
