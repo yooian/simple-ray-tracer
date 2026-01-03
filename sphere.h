351291f5d8dc254c72d26ff0f715edf78cb8c1e0
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 19:48:59 -0800

    Add material initialization to sphere hit logic

--- sphere.h
+++ sphere.h
@@ -6,7 +6,10 @@
 class sphere : public hittable
 {
 public:
-    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius))
+    {
+        // TODO: Initialize material pointer 'mat'
+    }
 
     bool hit(const ray &r, interval ray_t, hit_record &rec) const override
     {
@@ -36,6 +39,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -43,6 +47,7 @@ public:
 private:
     point3 center;
     double radius;
+    std::shared_ptr<material> mat;
 };
 
 #endif
