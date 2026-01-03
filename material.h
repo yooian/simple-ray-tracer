ed9aa830dfb4a22199d8e75fb353cd312ac6adb0
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 19:54:10 -0800

    Add lambertian scatter logic

--- material.h
+++ material.h
@@ -31,6 +31,10 @@ public:
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override
     {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
     }
 
 private:
