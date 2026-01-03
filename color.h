b5b9c147cd7f6ef862898b762fdabeaccc7c1e36
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 18:08:28 -0800

    Fix 255 value error

--- color.h
+++ color.h
@@ -13,9 +13,12 @@ void write_color(std::ostream &out, const color &pixel_color)
 
     // Translate [0,1] component values to rgb range [0,255]
     static const interval intensity(0.000, 0.999);
-    int rbyte = int(255.999 * intensity.clamp(r));
-    int gbyte = int(255.999 * intensity.clamp(g));
-    int bbyte = int(255.999 * intensity.clamp(b));
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
+
+    // since adding average of all samples, need to clamp values to prevent going
+    // beyond [0,1] range
 
     // Write out pixel components
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
