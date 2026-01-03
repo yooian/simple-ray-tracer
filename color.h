7f9982a5231d545b721a0730dc81fdb3b617fc23
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 19:31:09 -0800

    Add linear to gamma correction in write_color

--- color.h
+++ color.h
@@ -6,11 +6,24 @@
 
 using color = vec3;
 
+// accounting for gamma correction in our linear images
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component < 0)
+        return std::sqrt(linear_component);
+    return 0;
+}
+
 void write_color(std::ostream &out, const color &pixel_color)
 {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2 ^(1/gamma) = sqrt
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate [0,1] component values to rgb range [0,255]
     static const interval intensity(0.000, 0.999);
