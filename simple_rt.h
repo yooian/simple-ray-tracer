9c4f1480bc5202d63f610190c9667b1150964a40
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 17:59:47 -0800

    Add random_double functions

--- simple_rt.h
+++ simple_rt.h
@@ -2,6 +2,7 @@
 #define SIMPLE_RT_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -18,6 +19,18 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    // Return a random real number in [0,1)
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    // Return a random real number in [min,max)
+    return min + (max - min) * random_double();
 }
 
 // Common Headers
