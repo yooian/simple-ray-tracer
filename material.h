4cdea2a20fffd6b724a213b8ebf6954be9506a5b
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 20:01:14 -0800

    Add near zero check for scatter direction in materials

--- material.h
+++ material.h
@@ -33,6 +33,11 @@ public:
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override
     {
         auto scatter_direction = rec.normal + random_unit_vector();
+
+        // Catch near zero scatters to prevent degenerative behavior
+        if (scatter_direction.near_zero())
+            scatter_direction = rec.normal;
+
         scattered = ray(rec.p, scatter_direction);
         attenuation = albedo;
         return true;
