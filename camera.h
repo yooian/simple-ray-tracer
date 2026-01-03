a76ebbcf62d609369586214701e01e5f38b5ce18
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 19:24:45 -0800

    Add lambertian reflection diffuse instead of random

--- camera.h
+++ camera.h
@@ -110,7 +110,7 @@ private:
         hit_record rec;
         if (world.hit(r, interval(0.001, infinity), rec)) // 0.001 for shadow acne (error tolerance point just below surface)
         {
-            vec3 direction = random_on_hemisphere(rec.normal);
+            vec3 direction = rec.normal + random_unit_vector();
             return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
