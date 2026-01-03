07caece5cbc1db24a145133bff684e6e2d4a024d
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 21:07:34 -0800

    Update material left to dialectric:

--- main.cc
+++ main.cc
@@ -13,7 +13,7 @@ int main()
 
 	auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
 	auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	auto material_left = std::make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+	auto material_left = std::make_shared<dialectric>(1.50);
 	auto material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
 	// world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
