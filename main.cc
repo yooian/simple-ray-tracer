77702e9bb663f378c7eb3f5f1a73ee30a970c30b
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 20:38:08 -0800

    Add metal spheres to main

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
+#include "material.h"
 #include "sphere.h"
 
 int main()
@@ -10,8 +11,16 @@ int main()
 	// World
 	hittable_list world;
 
-	world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
+	auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	auto material_left = std::make_shared<metal>(color(0.8, 0.8, 0.8));
+	auto material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+
+	// world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100.0, material_ground));
+	world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+	world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	camera cam;
 
