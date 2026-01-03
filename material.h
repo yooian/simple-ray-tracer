b2040deb8aa05b1ce2249fa204e1c99843317ad4
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 20:26:06 -0800

    Add metal material class

--- material.h
+++ material.h
@@ -47,4 +47,20 @@ private:
     color albedo;
 };
 
+class metal : public material
+{
+public:
+    metal(const color &albedo) : albedo(albedo) {}
+
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override
+    {
+        vec3 reflected = reflect(r_in.direction, rec.normal);
+        scattered = ray(rec.p, reflected);
+        attenuation = albedo;
+        return true;
+    }
+
+private:
+    color albedo;
+};
 #endif
