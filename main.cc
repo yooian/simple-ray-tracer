13e1b1f9bd5e5d0f45e6f6cae7963c9c8673dbba
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 22:16:09 -0800

    Add wide-angle cam test:

--- main.cc
+++ main.cc
@@ -11,18 +11,25 @@ int main()
 	// World
 	hittable_list world;
 
-	auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	auto material_left = std::make_shared<dialectric>(1.50);
-	auto material_bubble = std::make_shared<dialectric>(1.00 / 1.50);
-	auto material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+	// auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	// auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	// auto material_left = std::make_shared<dialectric>(1.50);
+	// auto material_bubble = std::make_shared<dialectric>(1.00 / 1.50);
+	// auto material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-	// world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100.0, material_ground));
-	world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
-	world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-	world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
-	world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+	// // world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
+	// world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100.0, material_ground));
+	// world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+	// world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	// world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
+	// world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+
+	// Wide-angle test
+	auto R = std::cos(pi / 4);
+	auto material_left = std::make_shared<lambertian>(color(0, 0, 1));
+	auto material_right = std::make_shared<lambertian>(color(1, 0, 0));
+	world.add(std::make_shared<sphere>(point3(-R, 0, -1), R, material_left));
+	world.add(std::make_shared<sphere>(point3(R, 0, -1), R, material_right));
 
 	camera cam;
 
@@ -30,6 +37,8 @@ int main()
 	cam.image_width = 400;
 	cam.samples_per_pixel = 100;
 	cam.max_depth = 50;
+
+	cam.vfov = 90;
 
 	cam.render(world);
 }
