6f5561205eb77353b3e69dabe3620803cf60de0c
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 16:46:50 -0800

    Add ray_color from main

--- camera.h
+++ camera.h
@@ -22,9 +22,19 @@ private:
     }
 
     // we're moving ray_color() here
-    color ray_color(const ray &r, const hittable &world) const {
+    color ray_color(const ray &r, const hittable &world) const
+    {
+        hit_record rec;
+        if (world.hit(r, interval(0, infinity), rec))
+        {
+            return 0.5 * (rec.normal + color(1, 1, 1));
+        }
 
-    };
+        vec3 unit_direction = unit_vector(r.direction());
+        auto a = 0.5 * (unit_direction.y() + 1.0);                          // want to scale unit_d [-1,1] to [0,1] range
+        return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0); // linear interpolation white, blue
+    }
+};
 }
 
 #endif
