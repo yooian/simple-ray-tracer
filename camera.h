9abe5fb525f338d6e38233dde52970984aaf01a5
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 19:06:16 -0800

    Add tolerance for shadow acne

--- camera.h
+++ camera.h
@@ -108,7 +108,7 @@ private:
             return color(0, 0, 0);
 
         hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec))
+        if (world.hit(r, interval(0.001, infinity), rec)) // 0.001 for shadow acne (error tolerance point just below surface)
         {
             vec3 direction = random_on_hemisphere(rec.normal);
             return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
