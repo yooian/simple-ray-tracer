71dceadc63cf8c155275e1e9f5c1af51cad44af6
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 19:38:09 -0800

    Add basic abstract material class structure

--- material.h
+++ material.h
@@ -1,4 +1,17 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include "hittable.h"
+
+class material
+{
+public:
+    virtual ~material() = default;
+
+    virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const
+    {
+        return false;
+    };
+}
+
 #endif
