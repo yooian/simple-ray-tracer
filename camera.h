54ec94ec95c670bb1c5fdec0bcd21703e2c51906
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 19:01:16 -0800

    Add max ray bounce depth parameter

--- camera.h
+++ camera.h
@@ -13,6 +13,7 @@ public:
     double aspect_ratio = 1.0;  // Ratio image width/height
     int image_width = 100;      // In pixels
     int samples_per_pixel = 10; // Count of random samples for each pixel
+    int max_depth = 10;         // Max number of ray bounces into scene
 
     void render(const hittable &world)
     {
@@ -31,7 +32,7 @@ public:
                 for (int sample = 0; sample < samples_per_pixel; ++sample)
                 {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -100,13 +101,17 @@ private:
     }
 
     // we're moving ray_color() here
-    color ray_color(const ray &r, const hittable &world) const
+    color ray_color(const ray &r, int depth, const hittable &world) const
     {
+        // Base case for ray bounce depth
+        if (depth <= 0)
+            return color(0, 0, 0);
+
         hit_record rec;
         if (world.hit(r, interval(0, infinity), rec))
         {
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p, depth - 1, direction), world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
