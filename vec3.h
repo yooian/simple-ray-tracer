29cb74a8826291425c6208622de1c73841b85fc2
Ian Yoo <isyoo@ucdavis.edu>
2026-01-02 19:59:40 -0800

    Add near zero check to vec header

--- vec3.h
+++ vec3.h
@@ -53,6 +53,12 @@ public:
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
 
+    // Return true if the vector is close to zero in all dimensions
+    bool near_zero() const
+    {
+        auto s = 1e-8;
+        return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
+    }
     // Generating arbitrary random vectors
     static vec3 random()
     {
