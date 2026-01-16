3a7b7bfc12ba560c8a82cbfcbd3fc82f7ea68e84
Ian Yoo <isyoo@ucdavis.edu>
2026-01-15 16:59:44 -0800

    Add initial class structure

--- triangle.h
+++ triangle.h
@@ -1,4 +1,21 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#include "hittable.h"
+
+class triangle : public hittable
+{
+public:
+    triangle(const point3 &v1, const point3 &v2, const point3 &v3, std::shared_ptr<material> mat) : v1(v1), v2(v2), v3(v3), mat(mat) {}
+
+    bool hit(const ray &r, interval ray_t, hit_record &rec) const override
+    {
+        return true;
+    }
+
+private:
+    point3 v1, v2, v3;
+    std::shared_ptr<material> mat;
+};
+
 #endif
