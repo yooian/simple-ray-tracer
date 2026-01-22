3806165a5d649bfe887c0402db8e63556d5f72e2
Ian Yoo <isyoo@ucdavis.edu>
2026-01-21 16:56:15 -0800

    [fix] Triangle: add near_zero check and fix vertices naming

--- triangle.h
+++ triangle.h
@@ -6,7 +6,7 @@
 class triangle : public hittable
 {
 public:
-    triangle(const point3 &v1, const point3 &v2, const point3 &v3, std::shared_ptr<material> mat) : v1(v1), v2(v2), v3(v3), mat(mat)
+    triangle(const point3 &v0, const point3 &v1, const point3 &v2, std::shared_ptr<material> mat) : v0(v0), v1(v1), v2(v2), mat(mat)
     {
         // Equation of plane is Ax + By + Cz + D = 0
         // Where A, B, C are the components of the normal N
@@ -14,41 +14,47 @@ public:
         // D is offset.
 
         // Need normal of plane the triangle sits on
-        vec3 v1v2 = v1 - v2; // might need to flip for culling?
-        vec3 v1v3 = v1 - v3;
-        N = unit_vector(cross(v1v2, v1v3)); // N = plane normal
-        D = -dot(N, v1)
+        vec3 v0v1 = v0 - v1; // might need to flip for culling?
+        vec3 v0v2 = v0 - v2;
+        N = unit_vector(cross(v0v1, v0v2)); // N = plane normal
+        D = -dot(N, v0)
     }
 
     bool hit(const ray &r, interval ray_t, hit_record &rec) const override
     {
-        // Calculate P
+        // Calculate P and t
+        vec3 denom = dot(N, r.dir);
+
+        if (near_zero(denom))
+        {
+            return false;
+        }
 
         // Check interval of t
-        vec3 t = -(dot(N, r.orig) + D) / dot(N, r.dir);
+        vec3 t = -(dot(N, r.orig) + D) / denom;
         if (!ray_t.surrounds(t))
         {
             return false;
         }
 
         // Inside-outside test
+        vec3 v0v1 = v1 - v0;
         vec3 v1v2 = v2 - v1;
-        vec3 v2v3 = v3 - v2;
-        vec3 v3v1 = v1 - v3;
+        vec3 v2v0 = v0 - v2;
+
+        // Test edge v0 -> v1
+        vec3 v0p = rec.p - v0;
+        if (dot(N, cross(v0v1, v0p)) < 0)
+            return false; // point not in tri
 
         // Test edge v1 -> v2
         vec3 v1p = rec.p - v1;
         if (dot(N, cross(v1v2, v1p)) < 0)
-            return false; // point not in tri
-
-        // Test edge v2 -> v3
-        vec3 v2p = rec.p - v2;
-        if (dot(N, cross(v2v3, v2p)) < 0)
             return false;
 
-        // Test edge v3 -> v1
-        vec3 v3p = rec.p - v3;
-        if (dot(N, cross(v3v1, v3p)) < 0)
+        // Test edge v2 -> v0
+        vec3 v2p = rec.p - v2;
+        if (dot(N, cross(v2v0, v2p)) < 0)
             return false;
 
         // Write to hit record after all checks pass
@@ -61,7 +67,7 @@ public:
     }
 
 private:
-    point3 v1, v2, v3; // Gotta assume counter clockwise winding
+    point3 v0, v1, v2; // Gotta assume counter clockwise winding
     vec3 N;
     vec3 D;
     std::shared_ptr<material> mat;
